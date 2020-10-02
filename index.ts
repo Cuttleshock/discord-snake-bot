93f25f055b5642ba3477f4ac8267080937e3537a
Tori L <48884484+Cuttleshock@users.noreply.github.com>
2020-10-03 00:36:40 +0100

    Add message handler skeleton

--- index.ts
+++ index.ts
@@ -1,10 +1,32 @@
 import Discord from 'discord.js';
+
 import config from '../config-secrets/config-hw-bot.json';
+import * as vals from './values';
 
 const client = new Discord.Client();
 
 client.login(config.BOT_TOKEN);
 
+const shouldQuickExitMessage = (msg: Discord.Message) => {
+    if (msg.author.bot
+     || !msg.content.startsWith(vals.prefix)
+    ) return true;
+
+    return false;
+}
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', (msg: Discord.Message) => {
+    if (shouldQuickExitMessage(msg)) return;
+
+    const commandBody = msg.content.slice(vals.prefixLen);
+    const args = commandBody.split(' ');
+    const command = args.shift().toLowerCase();
+
+    switch (command) {
+        default:
+            break;
+    }
 });
---
+++ values.ts
@@ -0,0 +1,4 @@
+// Command prefixes
+export const prefix = '==';
+export const prefixLen = 2;
+

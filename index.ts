e52348d49776bc40413bf9d3100a8e2ef00cf314
Tori L <48884484+Cuttleshock@users.noreply.github.com>
2020-10-03 00:35:40 +0100

    console.log when client is ready

--- index.ts
+++ index.ts
@@ -4,3 +4,7 @@ import config from '../config-secrets/config-hw-bot.json';
 const client = new Discord.Client();
 
 client.login(config.BOT_TOKEN);
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});

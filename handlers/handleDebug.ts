f212d989d690e8dcb74c4228fdec509cd49a5dac
Tori L <48884484+Cuttleshock@users.noreply.github.com>
2021-12-18 15:33:40 +0000

    Change debug to print out member IDs

--- handlers/handleDebug.ts
+++ handlers/handleDebug.ts
@@ -1,32 +1,10 @@
 import { Command } from '@types';
-import bulkDerange, { proceduralDerange } from '../helpers/derange';
 
-const handleDebug: Command = async ({ msg, args }) => {
-    if (!process.env.DEBUG) {
-        msg.channel.send('no');
-        return;
-    }
-
-    if (args.length < 2) {
-        msg.channel.send('2 numbers pls');
-        return;
-    }
-
-    const setSize = parseInt(args[0]);
-    const repeats = parseInt(args[1]);
-    if (isNaN(setSize) || isNaN(repeats)) {
-        msg.channel.send('2 __numbers__ please');
-        return;
-    }
-
-    msg.channel.send('gotcha :snake:');
-    console.log('procedural:');
-    for (let i = 0; i < repeats; ++i) {
-        console.log(proceduralDerange(setSize));
-    }
-    console.log('bulk:');
-    for (let i = 0; i < repeats; ++i) {
-        console.log(bulkDerange(setSize));
+const handleDebug: Command = async ({ msg }) => {
+    const members = msg.mentions.members;
+    for (const m of members.values()) {
+        const user = await m.user.fetch();
+        console.log(`${user.tag}: ${user.id}`);
     }
 }
 
